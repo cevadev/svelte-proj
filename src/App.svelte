afe87b11c6b4800e768b8b07e543404b408903fd
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2020-10-01 21:55:18 -0300

    Curso Svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,54 @@
 <script>
+	import About from "./components/About.svelte";
+	import Text from "./components/Text.svelte";
+	import Person from "./components/Person.svelte"
+
 	export let name;
+	export let lastName;
+	let svelteLogo = "https://arepa.s3.amazonaws.com/svelte-logo.png";
+
+	const data = {
+		name: "Carlos",
+		lastName: "Villanueva",
+		age: 31,
+	};
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello {name} {lastName}!</h1>
+	<About />
+	<Text anotherText="Hello Svelte Community!!!"/>
+	<Person name={data.name} lastName={data.lastName} age={data.age}/>
+	<img src={svelteLogo} alt="Svelte logo">
 </main>
 
 <style>
+	html{
+		font-size: 62.5%;
+	}
+	:global(body){
+		background-color: #f2eee2;
+		color: #0084f6;
+	}
+	:global(root){
+		--theme-color: purple;
+	}
+
+	/*estructura css global para que se active el dark-mode al documento body*/
+	:global(body.dark-mode){
+		background-color: #1d3040;
+		color: #bfc2c7;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+
+	main img{
+		width: 30%;
 	}
 
 	h1 {
---
+++ src/components/About.svelte
@@ -0,0 +1,42 @@
+<script>
+    let someText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent felis velit, malesuada sed erat nec, vestibulum dictum dolor. Mauris eget iaculis justo. Maecenas eros dui, malesuada nec dapibus egestas, posuere ut metus. Fusce dignissim dapibus congue. Nulla lobortis, velit quis molestie condimentum, nulla tortor facilisis metus, sit amet ullamcorper risus mi vitae elit. Aenean volutpat dui urna, non efficitur neque maximus eget. Donec fringilla non libero dapibus mattis.`;
+
+    //manejamos un contador
+    let count = 0;
+    function handleClick(){
+        count += 1;
+    }
+
+    //Manejando condicionales
+    let styles = { darkMode: false }
+
+    //funcion que al dar click se activa el darkmode
+    function toogle(){
+        styles.darkMode = !styles.darkMode //si el valor de darkMode es false pasalo a true o viceversa
+
+        //Trabajamos con el DOM y activamos el selector .class dark-mode para aplicar los estilos al body
+        window.document.body.classList.toggle("dark-mode")
+    }
+</script>
+
+<style>
+    p{
+        color: var(--theme-color);
+    }
+</style>
+
+<div class="About">
+
+    <!--Condicional para mostrar o no el texto-->
+    {#if !styles.darkMode}
+        <p>{someText}</p>
+    {:else}
+        <p>Hello dark mode</p>
+    {/if}
+
+    
+    <button on:click={handleClick}>Click me! {count === 0 ? '' : count}</button>
+    
+    <!--boton que implementa la funcionalidad dark-mode-->
+    <button on:click={toogle}>DarkMode</button>
+</div>
